6ce6276c8375b9ee86a620694359d433034518b6
naturalstupiditynotAI <25b1227@iitb.ac.in>
2026-05-31 23:05:47 +0530

    Update alu.v

--- Week 2/Assignment/alu.v
+++ Week 2/Assignment/alu.v
@@ -6,8 +6,44 @@ module alu(
     input  [7:0]     a, b,
     input  [2:0]     op,
     output reg [7:0] result,
-    output           zero,
+    output reg       zero,
     output reg       carry,
     output reg       overflow
-);
+    );
+    reg [8:0] temp;
+    always@(*)
+    begin 
+    temp=9'b0;
+        case(op)
+            3'b000: 
+                begin 
+                    result = a+b;
+                    temp = a+b;  
+                end
+            3'b001: 
+                begin 
+                result = a-b;
+                temp = a-b; 
+                end
+            3'b010: result = a&b;
+            3'b011: result = a|b;
+            3'b100: result = a^b;
+            3'b101: result = a<< 1;
+            3'b110: result = a>> 1;
+
+        endcase
+
+          if(result==8'b0)
+              zero=1; 
+          else zero=0; 
+          
+          if(op==3'b000||op==3'b001)
+              carry=temp[8]; 
+          else carry=1'b0; 
+
+          if((op==3'b000&&a[7]==b[7]&&result[7]!=a[7])||(op==3'b001&&a[7]!=b[7]&&result[7]!=a[7]))
+              overflow=1; 
+          else overflow=0;
+          
+        end
 endmodule
