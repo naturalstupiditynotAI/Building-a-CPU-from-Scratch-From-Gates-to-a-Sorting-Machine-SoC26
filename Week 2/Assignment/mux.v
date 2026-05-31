2ab572355608f6a83e089d1d7b762ddf5eaa52d0
naturalstupiditynotAI <25b1227@iitb.ac.in>
2026-05-31 20:55:06 +0530

    Implement 2:1 and 4:1 MUX functionality

--- Week 2/Assignment/mux.v
+++ Week 2/Assignment/mux.v
@@ -13,13 +13,21 @@ module mux2 #(parameter WIDTH = 8) (
 );
     // YOUR CODE HERE
     // Hint: one line with assign and ternary operator is perfect here
+    assign y = sel ? b: a; 
 endmodule
 
 // 4:1 MUX — WIDTH bits wide
 module mux4 #(parameter WIDTH = 8) (
     input  [WIDTH-1:0] d0, d1, d2, d3,
     input  [1:0]       sel,
-    output [WIDTH-1:0] y
+    output reg [WIDTH-1:0] y
 );
     // YOUR CODE HERE
+    always@(*)
+    case(sel)
+        2'b00: y=d0; 
+        2'b01: y=d1; 
+        2'b10: y=d2; 
+        2'b11: y=d3; 
+    endcase
 endmodule
